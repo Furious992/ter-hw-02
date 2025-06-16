18aeafb210feb8f50eba9e6face6df3bab2d06f2
Furious992 <m.a.pertsev@yandex.ru>
2025-06-16 13:28:33 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
-###cloud vars
-
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
 
 variable "cloud_id" {
   type        = string
@@ -16,6 +18,7 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
@@ -28,47 +31,33 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
-variable "vm_web_family" {
-  type = string
-  default = "ubuntu-2004-lts"
-  description = "ubuntu version"
-}
-
-variable "vm_web_name" {
-  type = string
-  default = "netology-develop-platform-web"
-  description = "instance name"
-}
-
-variable "vm_web_platform_id" {
-  type = string
-  default = "standard-v1"
-  description = "platform ID"
-}
-
-variable "vm_web_cores" {
-  type = number
-  default = 2
-  description = "vCPU numbers"
-}
-
-variable "vm_web_memory" {
-  type = number
-  default = 1
-  description = "VM memory, GB"
-}
-
-variable "vm_web_core_fraction" {
-  type = number
-  default = 5
-  description = "core fraction"
-}
-
-
-###ssh vars
-
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "ssh-ed25519 AAAAC3N.......... maxp@max-VirtualBox"
   description = "ssh-keygen -t ed25519"
+}
+
+variable "vms_resources" {
+  description = "Resource for all vms"
+  type = map(map(any))
+  default = {
+    vm_web = {
+      core_fraction = 5
+      cores         = 2
+      memory        = 1
+    }
+    vm_db = {
+      core_fraction = 20
+      cores         = 2
+      memory        = 2
+    }
+  }
+}
+
+variable "vms_metadata" {
+  type = map(string)
+  default = {
+    "serial-port-enable" = "1"
+    "ssh-keys"           = "ubuntu:ssh-ed25519 AAAAC3Nz maxp@maxpsrv"
+  }
+  description = "Metadata for VMs"
 }

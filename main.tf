807256d9b376e1600d11f9536e9e2c680d90438c
Furious992 <m.a.pertsev@yandex.ru>
2025-06-16 13:46:41 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,34 +9,70 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+resource "yandex_compute_instance" "platform1" {
+  name        = local.vm_web_instance_name
+  platform_id = var.vm_web_platform_id
+  zone        = var.vm_web_zone
 
-data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
-}
-resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart"
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vms_resources.vm_web.cores
+    memory        = var.vms_resources.vm_web.memory
+    core_fraction = var.vms_resources.vm_web.core_fraction
   }
+
   boot_disk {
     initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
+      image_id = var.vm_web_image_id
+      type     = var.vm_web_disk_type
     }
   }
-  scheduling_policy {
-    preemptible = true
-  }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  metadata = var.vms_metadata
+
+  scheduling_policy {
+    preemptible = var.vm_web_preemptible
+  }
+}
+
+resource "yandex_compute_instance" "platform2" {
+  name        = local.vm_db_instance_name
+  platform_id = var.vm_db_platform_id
+  zone        = var.vm_db_zone
+
+  resources {
+    cores         = var.vms_resources.vm_db.cores
+    memory        = var.vms_resources.vm_db.memory
+    core_fraction = var.vms_resources.vm_db.core_fraction  
   }
 
+  boot_disk {
+    initialize_params {
+      image_id = var.vm_db_image_id
+      type     = var.vm_db_disk_type
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = var.vms_metadata
+
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+}
+
+output "external_ip_address" {
+  value = yandex_compute_instance.platform.network_interface.0.nat_ip_address
+}
+
+output "instance_id" {
+  value = yandex_compute_instance.platform.id
 }
